0ea77c2d4a523198a677cb29d807dd6ff2fd3176
phorch69 <phorch69@gmail.com>
2024-09-27 19:00:40 -0300

    Integracion de HDRs e interacciones

--- src/Component/three.js
+++ src/Component/three.js
@@ -6,11 +6,16 @@ import { FlyControls } from 'three/addons/controls/FlyControls.js';
 //import { FirstPersonControls } from 'three/addons/controls/FirstPersonControls.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+//import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+//import { UltraHDRLoader } from 'three/addons/loaders/UltraHDRLoader.js';
+//import { EXRLoader } from 'three/addons/loaders/EXRLoader.js';
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
+import { Water } from 'three/addons/objects/Water.js';
+import { Sky } from 'three/addons/objects/Sky.js';
 
 let camera, container, scene, renderer, stats, controls;
-let DracoLoader, Loader, Models;
-let zoom, Clock;
+let DracoLoader, Loader, Models, HDRLoader, HDR;
+let zoom, Clock, water;
 let cube, light, mesh;
 
 init ();
@@ -19,17 +24,22 @@ function init() {
 	container = document.getElementById( 'container' );
 
 	scene = new THREE.Scene();
-	scene.background = new THREE.Color(0X000000, 0X000000, 0);
+	//scene.background = new THREE.Color(0X000000, 0X000000, 0);
 
 	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-	camera.position.z = 5;
-	camera.position.y = 5;
+	camera.position.z = 50;
+	camera.position.y = 50;
+	camera.position.x = 50;
 
-	Objetos3d()
+	//Objetos3d();
+	Ilumination();
+	Loader3d();
 	render();
 	Zoom();
+	Oceano();
 	//Control libre
 	//Controls();
+	Background();
 
 	window.addEventListener('resize', onWindowResize, false);
 }
@@ -41,16 +51,23 @@ function render() {
 	container.appendChild( renderer.domElement );
 	stats = new Stats();
 
+	//renderer.toneMapping = new THREE.NeutralToneMapping;
+	//renderer.toneMappingExposure = 0.25;
+	renderer.shadowMap.enabled = true;
+	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
 	container.appendChild( stats.dom );
 }
+
 //Movimiento de la camara con mouse
 function Zoom() {
 	zoom = new OrbitControls(camera, renderer.domElement);
 	zoom.autoRotate = false;
-	zoom.zoomSpeed = 2;
-	zoom.minDistance = 2;
-	zoom.maxDistance = 8;
+	zoom.zoomSpeed = 3;
+	zoom.minDistance = 7;
+	zoom.maxDistance = 55;
 	zoom.maxPolarAngle = 1.4;
+	zoom.maxTargetRadius = 0;
 }
 //Movimien de la camara con teclados
 function Controls () {
@@ -63,32 +80,69 @@ function Controls () {
 	controls.handleResize();
 }
 
+
+
+function Background() {
+	//HDRLoader = new RGBELoader();
+    //HDRLoader.setDataType( THREE.FloatType );
+	//HDRLoader.setDataType( THREE[ HalfFloatType ] );
+    new RGBELoader().load( '/models/HDRs/Cielo.hdr', ( environmentMap ) => {
+
+        environmentMap.mapping = THREE.EquirectangularReflectionMapping;
+		//environmentMap.needsUpdate = true;
+
+		scene.background = environmentMap;
+		scene.environment = environmentMap;
+    } );
+}
+
+function Oceano() {
+	const waterGeometry = new THREE.PlaneGeometry( 10000, 10000 );
+
+	water = new Water( waterGeometry, {
+						textureWidth: 512,
+						textureHeight: 512,
+						waterNormals: new THREE.TextureLoader().load( '/models/textures/Water.jpg', function ( texture ) {
+
+						texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+
+						} ),
+						sunDirection: new THREE.Vector3(),
+						sunColor: 0xffffff,
+						waterColor: 0x001e0f,
+						distortionScale: 0.5,
+						fog: scene.fog !== undefined
+					}
+				);
+
+	water.rotation.x = - Math.PI / 2;
+
+	water.material.uniforms['time'].value += 0.01;
+
+	scene.add( water );
+}
+
 function Objetos3d () {
 	const geometry = new THREE.BoxGeometry( 15, 0.2, 15 );
 	geometry.position = new THREE.Vector3( 0, 0, 0 );
 	const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 	cube = new THREE.Mesh( geometry, material );
-
-	const light = new THREE.AmbientLight( 0x909090 ); // soft white light
-
-	scene.add( cube, light );
-
-	Loader3d();
+	scene.add( cube );
 }
 
 function Loader3d () {
-	DracoLoader = new DRACOLoader();
-	DracoLoader.setDecoderPath( 'https://www.gstatic.com/draco/versioned/decoders/1.5.7/' );
+	//DracoLoader = new DRACOLoader();
+	//DracoLoader.setDecoderPath( 'https://www.gstatic.com/draco/versioned/decoders/1.5.7/' );
 	/*Todavia no se que hace
 	DracoLoader.setDecoderConfiguration({type: 'js'});
 	Loader.setDRACOLoader(DracoLoader);*/
 
 	Loader = new GLTFLoader();
-	Loader.setDRACOLoader( DracoLoader );
+	//Loader.setDRACOLoader( DracoLoader );
 	Loader.load( '/models/gltf/Cubo-Textura.gltf', function ( gltf ) {
 		Models = gltf.scene
-		Models.position.set( 0, 1, 0 );
-		Models.scale.set( 0.5, 0.5, 0.5 );
+		Models.position.set( 0, 0, 0 );
+		Models.scale.set( 8, 0.1, 8 );
 		scene.add( Models );
 		//gltf.animations; // Array<THREE.AnimationClip>
 		//gltf.scene; // THREE.Group
@@ -96,6 +150,13 @@ function Loader3d () {
 		//gltf.cameras; // Array<THREE.Camera>
 		//gltf.asset; // Object
 	})
+}
+
+function Ilumination() {
+	const Ambient = new THREE.AmbientLight( 0xffffff, 0.1 ); // soft white light
+	const light = new THREE.DirectionalLight( 0xffffff, 5 );
+	light.position.set( 0, 10, 0 );
+	scene.add( Ambient, light );
 }
 
 function onWindowResize() {
@@ -107,8 +168,7 @@ function onWindowResize() {
 }
 
 function Animacion () {
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
+	
 }
 
 function animate() {
@@ -117,6 +177,7 @@ function animate() {
 	//Control Libre de la camara
 	//Clock = new THREE.Clock();
 	//controls.update( Clock.getDelta() );
+
 	zoom.update();
 	stats.update();
 
